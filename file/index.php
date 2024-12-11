3a8907b3528b6251bd448b6dedbe29fbd71620a7
Aldi Hermansyah <alher6555@gmail.com>
2024-12-11 12:46:04 +0700

    Add files via upload

---
+++ file/index.php
@@ -0,0 +1,58 @@
+<?php
+include("koneksi.php");
+
+// query untuk menampilkan data
+$sql = 'SELECT * FROM data_barang';
+$result = mysqli_query($conn, $sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <title>Data Barang</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Data Barang</h1>
+        <a href="tambah.php" class="btn">Tambah Barang</a>
+        <div class="main">
+            <table>
+                <tr>
+                    <th>Gambar</th>
+                    <th>Nama Barang</th>
+                    <th>Kategori</th>
+                    <th>Harga Jual</th>
+                    <th>Harga Beli</th>
+                    <th>Stok</th>
+                    <th>Aksi</th>
+                </tr>
+                <?php if ($result): ?>
+                <?php while ($row = mysqli_fetch_array($result)): ?>
+                <tr>
+                    <td><img src="<?= $row['gambar']; ?>" alt="<?= $row['nama']; ?>"></td>
+                    <td><?= $row['nama']; ?></td>
+                    <td><?= $row['kategori']; ?></td>
+                    <td><?= $row['harga_jual']; ?></td>
+                    <td><?= $row['harga_beli']; ?></td>
+                    <td><?= $row['stok']; ?></td>
+                    <td class="table-actions">
+                        <a href="ubah.php?id=<?= $row['id_barang']; ?>" class="ubah">Ubah</a>
+                        <a href="hapus.php?id=<?= $row['id_barang']; ?>" class="hapus"
+                            onclick="return confirm('Yakin mau di hapus?')">Hapus</a>
+                    </td>
+                </tr>
+                <?php endwhile; ?>
+                <?php else: ?>
+                <tr>
+                    <td colspan="7">Belum ada data</td>
+                </tr>
+                <?php endif; ?>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
